b91d88c4a71bc5e7cb7960271bbcead2d7ab7a59
HUCSr <953064195@qq.com>
2024-09-18 21:18:50 +0800

    日志

--- Server/Server.py
+++ Server/Server.py
@@ -6,38 +6,34 @@ import json
 import jieba
 import math
 import random
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
 
 import Sql
 
+
+# 记录操作日志
+log_folder = "log"
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+
+log_file = os.path.join(log_folder, "server.log")
+handler = TimedRotatingFileHandler(
+    log_file, when="midnight", interval=1, backupCount=7, encoding="utf-8", mode="a"
+)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("ServerLogger")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 # 存储所有客户端套接字的列表
 clients = []
 
 requestTimeList = {}
 usernames = {}
-
-lastMessage = ""
-
-
-def SaveAutoReply(message):
-    global lastMessage
-
-    if lastMessage != "":
-        if os.path.exists("AutoReply.json") == False:
-            reply = {}
-            with open("AutoReply.json", "w", encoding="utf-8") as t:
-                json.dump(reply, t)
-        with open("AutoReply.json", "r", encoding="utf-8") as t:
-            reply = json.load(t)
-
-        if reply.get(lastMessage) == None:
-            reply[lastMessage] = []
-
-        reply[lastMessage].append(message)
-        with open("AutoReply.json", "w", encoding="utf-8") as t:
-            json.dump(reply, t)
-
-    lastMessage = message
 
 
 def handle_client(client_socket):
@@ -77,12 +73,16 @@ def handle_client(client_socket):
                 )
                 print("AWA")
                 sendMessage("001|" + str(result), client_socket)
+                logger.info(message[4:].split(" ")[0] + " login")
+
             # 注册请求
             elif message[:3] == "002":
                 result = Sql.Register(
                     message[4:].split(" ")[0], message[4:].split(" ")[1]
                 )
                 sendMessage("002|" + str(result), client_socket)
+                logger.info(message[4:].split(" ")[0] + " requests registration")
+
             # 获取仓库列表请求
             elif message[:3] == "003":
                 print(message[:3])
@@ -93,11 +93,15 @@ def handle_client(client_socket):
             elif message[:3] == "013":
                 result = Sql.add_warehouse(message[4:])
                 sendMessage("013|", client_socket)
+                logger.info("The " + str(message[4:]) + " warehouse has been added")
+
                 pass
             # 删除仓库
             elif message[:3] == "023":
                 result = Sql.del_warehouse(message[4:])
                 sendMessage("023|", client_socket)
+                logger.info("The " + str(message[4:]) + " warehouse has been deleted")
+
                 pass
             # 获取仓库物品
             elif message[:3] == "033":
@@ -108,11 +112,34 @@ def handle_client(client_socket):
             elif message[:3] == "043":
                 result = Sql.add_item(message[4:].split(" "))
                 sendMessage("043|", client_socket)
+                logger.info(
+                    "Added "
+                    + str(message[4:].split(" ")[2])
+                    + " items of "
+                    + str(message[4:].split(" ")[1])
+                    + " to the "
+                    + str(message[4:].split(" ")[0])
+                    + " warehouse, with a remark of "
+                    + str(message[4:].split(" ")[3])
+                    + "."
+                )
+
                 pass
             # 出库
             elif message[:3] == "053":
                 result = Sql.del_item(message[4:].split(" "))
                 sendMessage("053|", client_socket)
+                logger.info(
+                    "Deleted "
+                    + str(message[4:].split(" ")[2])
+                    + " items of "
+                    + str(message[4:].split(" ")[1])
+                    + " to the "
+                    + str(message[4:].split(" ")[0])
+                    + " warehouse, with a remark of "
+                    + str(message[4:].split(" ")[3])
+                    + "."
+                )
                 pass
             # 用户
             elif message[:3] == "004":
@@ -123,6 +150,12 @@ def handle_client(client_socket):
             elif message[:3] == "014":
                 result = Sql.change_role(message[4:].split(" "))
                 sendMessage("014|", client_socket)
+                logger.info(
+                    "User "
+                    + str(message[4:].split(" ")[0])
+                    + "'s permissions have been changed to "
+                    + str(message[4:].split(" ")[1])
+                )
                 pass
             # 获取权限
             elif message[:3] == "024":
